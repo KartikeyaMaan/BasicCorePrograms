e3d0d42a641643c511b600c2361c2b8897667260
KartikeyaMaan <kartikeya.maan1997@gmail.com>
2023-02-25 08:39:39 +0530

    ADDED : code func for Head or Tail

--- src/Core.java
+++ src/Core.java
@@ -1,5 +1,20 @@
 public class Core {
+
+    public static void headOrTail(int numberOfFlips) {
+        int tailCount = 0, headCount = 0;
+        for (int i = 1; i <= numberOfFlips; i++) {
+            if (Math.random() < 0.5)
+                tailCount++;
+            else
+                headCount++;
+        }
+        int headPercent = ((headCount * 100) / (headCount + tailCount));
+        int tailPercent = ((tailCount * 100) / (headCount + tailCount));
+        System.out.println("Head Percentage = " + headPercent);
+        System.out.println("Tail Percentage = " + tailPercent);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to basic core programs");
+        headOrTail(10);
     }
 }
